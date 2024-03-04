10af132c9d76b27219b95f26ed6ddaf1fa6d33b0
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2024-03-04 11:45:18 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # clines
+
+## A program for counting number of lines of code in projects
+
+## Table of contents: 
+- [setup](#setup)
+- [running](#running)
+    - [file_path](#file_path)
+    - [-i, --ignore regex](#i---ignore-regex)
+    - [-v, --verbose](#v---verbose)
+
+## setup
+
+There's no much rare packages here, it's just argparse in the requirements.txt file. You can install with the following command if you have pip installed on your machine
+
+    pip install -r requirements.txt
+
+## Running
+
+### file_path
+
+The application needs one required path which is file_path. This don't have to be a file, it can be a directory also and the program will count lines in all files in the directory
+
+### -i, --ignore regex
+
+I don't know about you but I don't want the app to spend the whole day counting code lines in node_modules. So, there's an option to ignore some files by typing a regex like *.lnk to ignore shortcuts. 
+
+you can open main.py and see hardcoded extensions and folders there, and whatever you type in will be appended to them. If you want to remove one of them, i'm sorry for this awful user experience but you have to edit them manually from the main file itself.
+
+So, a command example using it will be like
+
+    ./main.py project -i node_modules -i useless -i .*
+
+### -v, --verbose
+
+You're probably asking, what could go wrong with a program that just count some lines? Well, so sometimes when I use the app it throws errors because the encoding of the file is not utf-8. So, in order to know which folder/file cause the problem, I add this argument to inform you which file the application will do right now
+
+    python main.py -v
+
---
+++ main.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+
+
+from sys import exit
+from argparse import ArgumentParser
+import os
+import re
+# from setup import lines
+
+parser = ArgumentParser(description="Application for counting lines in files")
+parser.add_argument("file_path", action="append", help="The path for the file to count lines")
+parser.add_argument("-i", "--ignore", metavar="", action="append", help="Files or dirs to ignore", default=["^\/?(?:[^/]+\/)*(\.[^/]+)$", "node_modules", "^.*\.(jpg|mp4|MP4|jpeg|JPG|gif|GIF|doc|DOC|pdf|PDF|lnk|png|bak|pypam|pam|log|csv|txt)$", "__pycache__", "package-lock.json"])
+parser.add_argument("-v", "--verbose", action="store_true")
+
+args = parser.parse_args()
+
+if args.verbose:
+    verprint = print
+else:
+    verprint = lambda x : x
+
+
+def main():
+    line_count = 0
+    
+    files = get_paths(args.file_path)
+    if len(args.file_path) == 1:
+        project_name = os.path.split(args.file_path[0])[-1]
+    else:
+        project_name = "unknown"
+    
+    space_lines = 0
+    comments_sum = 0
+    sum = 0
+    for file in files:
+        verprint(f"Currently at file {file}")
+        line_count = 0
+        comments = 0
+        try:
+            with open(file, encoding='utf-8') as f:
+                for line in f.readlines():
+                    if line.strip().startswith(('#', '//', '/*', '*/', '"""', '--')):
+                        comments += 1
+                        continue
+                    if not line.isspace():
+                        line_count += 1
+                        continue
+                    space_lines += 1
+        except FileNotFoundError:
+            exit("File not found")
+        
+        sum += line_count
+        comments_sum += comments
+        print(f"Lines in {file}: {line_count}")
+        verprint(f"comments in {file}: {comments}")
+
+    print(f"\n\nLines in {project_name}:\n")
+    print(f"Sum of code lines: {sum}")
+    print(f"Sum of comments: {comments_sum}")
+    print(f"sum of lines written in the project: {sum + comments_sum}")
+    print(f"White space lines: {space_lines}")
+    
+def get_paths(paths):
+    new = []
+    for ignore in args.ignore:
+        r = re.compile(ignore)
+        paths = list(filter(lambda x : not r.match(os.path.split(x)[-1]), paths))
+        
+    for file in paths:
+        if os.path.isdir(file):
+            new += get_paths(map(lambda x : os.path.join(file, x), os.listdir(file)))
+            continue
+        new.append(file)
+    
+    return new
+
+
+if __name__ == "__main__":
+    main()
